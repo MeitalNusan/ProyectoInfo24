aab671ef38281f5e7eabf15ff866335a36c5f903
Meital <meiti2002@hotmail.com>
2024-10-16 20:21:56 -0300

    .

--- projectInfo/src/App.jsx
+++ projectInfo/src/App.jsx
@@ -1,3 +1,30 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from './Home/Home';
+import { Layout } from './components/Layout/layout';
+ 
+const App = () => {
+
+
+  return (
+    <>
+    <BrowserRouter>
+          <Routes>
+            <Route element={<Layout/>}>
+              <Route path="/" element={<Home />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+
+
 // import { useState } from 'react'
 // import { Playbar } from './EjerciciosClase/Playbar/playbar';
 // import './App.css'
@@ -49,29 +76,4 @@
 // export default App;
 
 
-
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Home } from './Home/Home';
-import { Layout } from './components/Layout/layout';
  
-const App = () => {
-
-
-  return (
-    <>
-    <BrowserRouter>
-          <Routes>
-            <Route element={<Layout/>}>
-              <Route path="/" element={<Home />} />
-            </Route>
-          </Routes>
-        </BrowserRouter>
-    </>
-  )
-}
-
-export default App
